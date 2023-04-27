6979fdf71f8c4c376659f154a1d9b1c2e790152f
John Savard <john.savard@springventuregroup.com>
2023-04-27 14:58:58 -0500

    Added functionality to move folders from one column to another

--- frontend/src/folder/subFolder/CreateSubFoldersPage.tsx
+++ frontend/src/folder/subFolder/CreateSubFoldersPage.tsx
@@ -2,11 +2,15 @@ import React, { useState } from 'react'
 import { InitialData } from './InitialData'
 import Column from './Column'
 import {DragDropContext, DropResult} from 'react-beautiful-dnd'
-import { Container } from '@mui/material'
+import { Button, Container } from '@mui/material'
 
 function CreateSubFoldersPage() {
 
     const [foldersAndColumns, setFoldersAndColumns] = useState(InitialData)
+
+    const handleSplitFolders = (event: any) => {
+        console.log(JSON.stringify(foldersAndColumns.columns))
+    }
 
     const handleOnDragEnd = (result: DropResult) => {
 
@@ -22,25 +26,55 @@ function CreateSubFoldersPage() {
             return;
         }
 
+        const start = foldersAndColumns.columns[source.droppableId as keyof typeof foldersAndColumns.columns];
+        const finish = foldersAndColumns.columns[destination.droppableId as keyof typeof foldersAndColumns.columns];
+
+        // Moving within the same column
+        if (start === finish){
+            const newFolderIds = Array.from(start.folderIds)
+
+            newFolderIds.splice(source.index, 1);
+            newFolderIds.splice(destination.index, 0, draggableId);
 
 
-        const column = foldersAndColumns.columns[source.droppableId as keyof typeof foldersAndColumns.columns];
-        const newFolderIds = Array.from(column.folderIds)
+            const newColumn = {
+                ...start,
+                folderIds: newFolderIds,
+            };
 
-        newFolderIds.splice(source.index, 1);
-        newFolderIds.splice(destination.index, 0, draggableId);
+            const newState = {
+                ...foldersAndColumns,
+                columns: {
+                    ...foldersAndColumns.columns,
+                    [newColumn.id]: newColumn
+                },
+            };
 
+            setFoldersAndColumns(newState);
+            return;
+        }
 
-        const newColumn = {
-            ...column,
-            folderIds: newFolderIds,
+        // Moving from one list to another
+        const startFolderIds = Array.from(start.folderIds)
+        startFolderIds.splice(source.index, 1);
+        const newStart = {
+        ...start,
+        folderIds: startFolderIds,
+        };
+
+        const finishFolderIds = Array.from(finish.folderIds);
+        finishFolderIds.splice(destination.index, 0, draggableId);
+        const newFinish = {
+        ...finish,
+        folderIds: finishFolderIds,
         };
 
         const newState = {
             ...foldersAndColumns,
             columns: {
                 ...foldersAndColumns.columns,
-                [newColumn.id]: newColumn
+                [newStart.id]: newStart,
+                [newFinish.id]: newFinish,
             },
         };
 
@@ -48,16 +82,24 @@ function CreateSubFoldersPage() {
     };
 
     return (
-        <DragDropContext
-            onDragEnd={handleOnDragEnd}>
-            <Container sx={{display: 'flex'}}>
-            {foldersAndColumns.columnOrder.map(columnId => {
-                const column = foldersAndColumns.columns[columnId as keyof typeof foldersAndColumns.columns];
-                const initialFolders = column.folderIds.map(folderId => foldersAndColumns.folders[folderId as keyof typeof foldersAndColumns.folders]);
-                return <Column key={column.id} column={column} folders={initialFolders} />
-            })}
-            </Container>
-        </DragDropContext>
+        <>
+            <DragDropContext
+                onDragEnd={handleOnDragEnd}>
+                <Container sx={{display: 'flex'}} disableGutters>
+                {foldersAndColumns.columnOrder.map(columnId => {
+                    const column = foldersAndColumns.columns[columnId as keyof typeof foldersAndColumns.columns];
+                    const initialFolders = column.folderIds.map(folderId => foldersAndColumns.folders[folderId as keyof typeof foldersAndColumns.folders]);
+                    return <Column key={column.id} column={column} folders={initialFolders} />
+                })}
+                </Container>
+            </DragDropContext>
+            <Button 
+            variant="contained"
+            onClick={handleSplitFolders}
+            color='success'
+            sx={{marginTop:'15px'}}>Split Folders
+            </Button>
+        </>
     )
 }
 
