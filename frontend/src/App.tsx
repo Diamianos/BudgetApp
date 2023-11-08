114f4c3dfb69a7625396af7b2b91e1ae99aa299a
John Savard <john.savard@springventuregroup.com>
2023-11-08 08:27:44 -0600

    Moving some pages around for clarity

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Container } from "@mui/system";
-
 import FoldersPage from "./pages/folders/FolderPage";
 import SubFoldersPage from "./pages/subFolders/SubFolderPage";
 import Navbar from "./components/Navbar";
-import CreateSubFoldersPage from "./pages/subFolders/CreateSubFoldersPage";
+import CreateSubFoldersPage from "./pages/subFolderCreation/CreateSubFoldersPage";
 
 function App() {
 	return (
